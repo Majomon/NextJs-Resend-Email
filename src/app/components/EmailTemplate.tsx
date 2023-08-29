7fcbdcc9ae4188c8281cf452de0576b04633e066
Mauri <mauri.monzon91@gmail.com>
2023-08-29 10:34:08 -0300

    Template y estructura de la ruta send

--- src/app/components/EmailTemplate.tsx
+++ src/app/components/EmailTemplate.tsx
@@ -9,5 +9,14 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
 }) => (
   <div>
     <h1>Welcome, {firstName}!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+      voluptatum quidem quia non dolorem reprehenderit doloremque accusamus esse
+      dolorum libero, explicabo cumque rerum nostrum debitis quam et incidunt
+      ea? Voluptates.
+    </p>
+    <button className="bg-sky-500 px-3 py-2">
+      <a href="https://porfolio-three-mocha.vercel.app/">Porfolio</a>
+    </button>
   </div>
 );
